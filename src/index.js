2671e9a999ea73ab4ad066f5a8a62a65119ef1b4
Enoch Nyako <nyakoapps@gmail.com>
2019-12-20 02:14:27 +0000

    use redux-persist to cache redux state

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
-import {store} from "./store";
+import { PersistGate } from 'redux-persist/integration/react';
+import store from "./store";
 
-ReactDOM.render(<Provider store={store} >
+ReactDOM.render(<Provider store={store.store} >
+    <PersistGate loading={null} persistor={store.persistor}>
     <App/>
+    </PersistGate>
 </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
--- src/store.js
+++ src/store.js
@@ -1,4 +1,16 @@
 import {combineReducers, createStore} from "redux";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 import products from "./reducers/products";
 
-export const store = createStore(combineReducers({products}));
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+const rootReducer = combineReducers({products});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+let store = createStore(persistedReducer);
+let persistor = persistStore(store);
+
+export default {store, persistor}
